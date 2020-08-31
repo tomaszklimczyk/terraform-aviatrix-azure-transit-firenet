65f12f55ef82e35a7f0492ef8033077bf9b589b5
Tomasz Klimczyk <tomasz.klimczyk@bigswitch.com>
2020-08-31 14:32:02 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ resource "aviatrix_firewall_instance" "firewall_instance" {
   egress_subnet          = aviatrix_vpc.default.subnets[0].cidr
   firenet_gw_name        = aviatrix_transit_gateway.single[0].gw_name
   username               = local.is_checkpoint ? "admin" : var.firewall_username
-  password               = local.is_checkpoint ? var.checkpoint_password : ""
+  password               = var.firewall_password
   management_subnet      = aviatrix_vpc.default.subnets[2].cidr
 }
 
@@ -69,7 +69,7 @@ resource "aviatrix_firewall_instance" "firewall_instance_1" {
   egress_subnet          = aviatrix_vpc.default.subnets[0].cidr
   firenet_gw_name        = aviatrix_transit_gateway.ha[0].gw_name
   username               = local.is_checkpoint ? "admin" : var.firewall_username
-  password               = local.is_checkpoint ? var.checkpoint_password : ""
+  password               = var.firewall_password
   management_subnet      = aviatrix_vpc.default.subnets[2].cidr
 }
 
@@ -83,7 +83,7 @@ resource "aviatrix_firewall_instance" "firewall_instance_2" {
   egress_subnet          = aviatrix_vpc.default.subnets[1].cidr
   firenet_gw_name        = "${aviatrix_transit_gateway.ha[0].gw_name}-hagw"
   username               = local.is_checkpoint ? "admin" : var.firewall_username
-  password               = local.is_checkpoint ? var.checkpoint_password : ""
+  password               = var.firewall_password
   management_subnet      = aviatrix_vpc.default.subnets[3].cidr
 }
 
